2b1fb78748d5eae21fa629293241e8d57161977f
pavan1011 <pavankumar.karkekopp@ucalgary.ca>
2020-02-13 13:45:35 -0700

    Initial Commit

---
+++ src/CalculateArea.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class CalculateArea {
+
+	private static float calcArea(float radius) {
+		// TODO Auto-generated method stub
+		return (float) (Math.PI * radius * radius);
+	}
+
+}
